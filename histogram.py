8f38bb4a9286c201dde8447e76785edb9cd04197
Bihan-tifr <61841616+Bihan-tifr@users.noreply.github.com>
2020-05-31 18:44:35 +0530

    Add files via upload

---
+++ histogram.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.loadtxt("data.txt")
+
+plt.title("Random numbers drawn from exponential pdf")
+plt.xlabel("x")
+plt.ylabel("frequency")
+plt.hist(x,density=True,bins=50)
+plt.savefig("p4_plot.jpg")
+plt.show()
+
---
+++ p4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main(void)
+{
+	long int i,n;
+	double x,y;
+	n=10000;
+	FILE*fp;
+	fp=fopen("data.txt","w");
+	for(i=0;i<=n;i++)
+	{
+		x=(double)rand()/(double)RAND_MAX;
+		y=-log(x);
+		fprintf(fp,"%lf\n",y);
+	}
+	fclose(fp);
+	system("python histogram.py");
+	return 0;
+}
+
